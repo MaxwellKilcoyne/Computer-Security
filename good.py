a8df28eb916678186f09f4ad2d954b749b3a4d5a
MaxwellKilcoyne <89420144+MaxwellKilcoyne@users.noreply.github.com>
2023-11-20 15:06:51 -0700

    Delete good.py

--- good.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# coding: latin-1
-blob = """AAAAAAAAAAAAAAA®i}Æ|TVãL¥¬¿@Ú?`è,:@ð¥/"nxÍª/¥úò¤ãPc¿YÏH3èeYúN$sL7¡f{¨äÅfîùÃ>Íð˜’5`IÅúTMtuæ>jÑ~Íìþñ¸{¦Êÿ=j"M—ÈÄåè[¢Z±›u§Íë.Ì¤û"""
-from hashlib import sha256
-print(sha256(blob.encode("latin-1")).hexdigest())
